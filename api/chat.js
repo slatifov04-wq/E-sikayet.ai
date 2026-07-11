e08771ab5923e1fcbeb65f94d9bcdfd61c31de01
slatifov04-wq <slatifov04@gmail.com>
2026-07-11 15:09:29 +0400

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -37,11 +37,11 @@ export default async function handler(req, res) {
 
     const data = await response.json();
 
-    if (!response.ok) {
-      return res.status(500).json({
-        reply: data.error?.message || "OpenRouter xətası"
-      });
-    }
+  if (!response.ok) {
+  return res.status(500).json({
+    reply: JSON.stringify(data)
+  });
+}}
 
     return res.status(200).json({
       reply: data.choices?.[0]?.message?.content || "Cavab alınmadı"
